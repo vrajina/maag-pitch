9a0d2d95dd8eafe8865a60867b7450497ed00063
vrajina <vrajina@gmail.com>
2026-06-24 17:22:38 +0300

    content: hero title MAAG + AWG = ОНЛАЙН!

--- patch.mjs
+++ patch.mjs
@@ -1,5 +1,8 @@
 import { readFileSync, writeFileSync } from 'fs';
 let f = readFileSync('src/pages/index.astro', 'utf-8');
-f = f.replace('~5\u00a0млрд\u00a0₽', '4,5+\u00a0млрд\u00a0₽');
+f = f.replace(
+  '<h1>MAAG <span>×</span> ОНЛАЙН</h1>',
+  '<h1>MAAG <span>+</span> AWG <span>=</span><br>ОНЛАЙН!</h1>'
+);
 writeFileSync('src/pages/index.astro', f);
 console.log('done');
